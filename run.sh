f63b420046238856faa21d37710e82a0c967ac05
Andrei Gherghescu <g.andrei.n@gmail.com>
2017-12-12 15:49:36 +0100

    Updated run script to check for image user

--- run.sh
+++ run.sh
@@ -10,13 +10,21 @@ set -e
 
 IMAGE_NAME=$1 && shift 1
 
+# Deterimine user of the docker image
+docker_user=$(docker image inspect --format '{{.Config.User}}' $IMAGE_NAME)
+if [ "$docker_user" = "" ]; then
+    home_folder="/root"
+else
+    home_folder="/home/$docker_user"    
+fi
+
 # Run the container with shared X11, shared network interface
 docker run --rm \
   --net=host \
   --ipc=host \
   --privileged \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -v $HOME/.Xauthority:/root/.Xauthority -e XAUTHORITY=/root/.Xauthority \
+  -v $HOME/.Xauthority:$home_folder/.Xauthority -e XAUTHORITY=$home_folder/.Xauthority \
   -e DISPLAY=$DISPLAY \
   -v $HOME/Projects/devs/ROS:/extern/ROS \
   -it $IMAGE_NAME "$@"
