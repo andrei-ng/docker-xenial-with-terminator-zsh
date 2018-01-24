781f4ddc8d96a0aa92e28d255452ebc29ab78667
Andrei Gherghescu <g.andrei.n@gmail.com>
2018-01-24 11:44:51 +0100

    Gave better name to container mapping folder

--- nvidia-opengl/entrypoint.sh
+++ nvidia-opengl/entrypoint.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 # Re-mount the docker volumes mounted at runtime in the 'extern' volume
-# to a 'data' folder inside the container user's HOME folder
-sudo mkdir -p /home/$USER/data
+# to a 'host_files' folder inside the container user's HOME folder
+sudo mkdir -p /home/$USER/host_files
 # Do a recursive mount
-sudo mount --rbind /extern $HOME/data
+sudo mount --rbind /extern $HOME/host_files
 
 # Change owner from root
-sudo chown $USER:$USER -R $HOME/data/
+sudo chown $USER:$USER -R $HOME/host_files/
 
 exec $@
