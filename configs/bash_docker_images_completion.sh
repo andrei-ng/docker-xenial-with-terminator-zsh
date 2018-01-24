284f60c6bee970bfb056af6f9031bb1f28958979
Andrei Gherghescu <g.andrei.n@gmail.com>
2018-01-24 09:52:15 +0100

    Cleaned bash completion script

--- configs/bash_docker_images_completion.sh
+++ configs/bash_docker_images_completion.sh
@@ -1,38 +1,5 @@
 # Andrei Gherghescu 23012018
 # Bash completion for custom run.sh script used to launch a docker image
-# First method works fine, but it is more involved. 
-# Second method is shorter. 
-
-# _docker_autocomplete_image_and_tags() {
-#     local cur
-#     COMPREPLY=()
-#     cur="${COMP_WORDS[COMP_CWORD]}"
-#     prev=${COMP_WORDS[COMP_CWORD-1]}
-#     __ltrim_colon_completions "$cur"
-
-#     local image_name
-
-#     if [[ $COMP_CWORD -eq 1 ]]; then
-#       local reposAndTags="$(docker images | awk 'NR>1 && $1 != "<none>" { if ($2 == "latest") print $1; else print $1":"$2 }')"
-#       COMPREPLY=( $(compgen -W "$reposAndTags" -- "$cur") ) #; compopt -o nospace;
-#       return
-#     fi
-
-#     if [[ $COMP_CWORD -eq 2 && ${cur}==:* ]]; then
-#       local imageTags="$(docker images $prev | awk 'NR>1 && $1 != "<none>" && $2 != "latest" { print $2}')"
-#       COMPREPLY=( $(compgen -W "$imageTags") ) #; compopt -o nospace;
-#       return
-#     fi
-
-#     if [[ $COMP_CWORD -eq 3 && ${prev}==:* ]]; then
-#       image_name=${COMP_WORDS[COMP_CWORD-2]}
-#       local imageTags="$(docker images $image_name | awk 'NR>1 && $1 != "<none>" { print $2}')"
-#       COMPREPLY=( $(compgen -W "$imageTags" -- "$cur")) #; compopt -o nospace;
-#       return
-#     fi
-# }
-
-# complete -F _docker_autocomplete_image_and_tags ./run.sh
 
 _docker_autocomplete_image_and_tags () {
   local cur="${COMP_WORDS[COMP_CWORD]}"
